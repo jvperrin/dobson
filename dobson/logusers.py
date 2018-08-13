a2e6f5813bc3ef18d03fd42eb45f39033d9d3038
Sean Lobo <seanlobo2010@gmail.com>
2018-08-12 20:10:09 -0700

    Add time to log entires for MAC Addresses

--- dobson/logusers.py
+++ dobson/logusers.py
@@ -1,3 +1,5 @@
+import datetime
+
 import dobson.utils as utils
 
 
@@ -5,8 +7,9 @@ def log_mac_addresses():
     """Logs all mac addresses connected to the router into a file"""
     mac_address = list([mac.lower() for mac in utils.get_mac_addresses()])
     log_str = ' '.join(mac_address)
+    this_minute = datetime.datetime.now().replace(second=0, microsecond=0).isoformat()
     with open(utils.MAC_LOG_FILE, 'a') as f:
-        f.write(log_str + '\n')
+        f.write(f"{this_minute} {log_str}\n")
 
 
 if __name__ == '__main__':
