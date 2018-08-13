80be542e19b6a5951337abd2f72f12a8c078ca71
Sean Lobo <seanlobo2010@gmail.com>
2018-08-12 20:14:09 -0700

    Remove unnecessary list() call

--- dobson/logusers.py
+++ dobson/logusers.py
@@ -5,7 +5,7 @@ import dobson.utils as utils
 
 def log_mac_addresses():
     """Logs all mac addresses connected to the router into a file"""
-    mac_address = list([mac.lower() for mac in utils.get_mac_addresses()])
+    mac_address = [mac.lower() for mac in utils.get_mac_addresses()]
     log_str = ' '.join(mac_address)
     this_minute = datetime.datetime.now().replace(second=0, microsecond=0).isoformat()
     with open(utils.MAC_LOG_FILE, 'a') as f:
